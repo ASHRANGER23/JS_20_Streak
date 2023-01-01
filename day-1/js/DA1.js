26d08083b88523bcdd94e0e0cbee53d08790698e
ASHRANGER23 <78732081+ASHRANGER23@users.noreply.github.com>
2023-01-01 23:48:37 +0530

    Add files via upload

---
+++ day-1/js/DA1.js
@@ -0,0 +1,18 @@
+let firstName = 'Ashish Noel'    
+let lastName = 'Moses'     
+let country = 'India'      
+let city = 'Delhi'         
+let age = 19
+let isMarried = false
+
+const gravity = 9.8        
+const boilingPoint = 100   
+const PI = 3.14          
+
+let name = 'Ashish Noel Moses',     
+  job = 'Pursuing Under graduate',
+  live = 'India'
+
+console.log(firstName, lastName, country, city, age, isMarried) 
+console.log(gravity, boilingPoint, PI) 
+console.log(name, job, live)
